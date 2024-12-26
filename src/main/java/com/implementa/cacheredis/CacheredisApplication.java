3ef1ca73b28cdd0060569b3e65dafc5e626d8e7e
Francilio Alencar de Sousa <francilioalencar@gmail.com>
2024-12-25 23:27:07 -0300

    Envio inicial do projeto

---
+++ src/main/java/com/implementa/cacheredis/CacheredisApplication.java
@@ -0,0 +1,13 @@
+package com.implementa.cacheredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheredisApplication.class, args);
+	}
+
+}
